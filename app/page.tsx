5db21f74bee9d2921c102489bfe5e68e6d6ab7b9
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-27 17:12:19 +0100

    server action in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Banner from "@/components/Banner";
 import Post from "@/components/Post";
-import PostFormAction from "@/components/server/post_form_action";
+import PostForm from "@/components/client/PostForm";
 import { createClient } from "@/utils/supabase/server";
+import { randomUUID } from "crypto";
 import { cookies } from "next/headers";
 
 export default async function Index() {
@@ -11,10 +12,36 @@ export default async function Index() {
   const { data } = await supabase.auth.getUser();
   const { user } = data;
 
+  async function submitPost(formData: FormData) {
+    "use server";
+
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    const { data } = await supabase.auth.getUser();
+    const { user } = data;
+
+    const post = String(formData.get("post"));
+
+    if (!post) return;
+    if (!user) return;
+
+    const { data: postData, error } = await supabase.from("posts").insert({
+      user_id: user.id,
+      text: post,
+      id: randomUUID(),
+    });
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+  }
+
   return (
     <>
       <section className="lg:max-w-2xl mx-auto lg:border-x lg:border-x-secondary/20">
-        {user ? <PostFormAction /> : null}
+        {user ? <PostForm submitPost={submitPost} /> : null}
         <Post />
         <Post />
         <Post />
